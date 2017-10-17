14e6962d4ab37e0f5b1402c7eb1c0533d86f095a
pyksoft <pyksoft@gmail.com>
2017-10-17 15:44:40 +1100

    Add google authentication

--- Gemfile
+++ Gemfile
@@ -44,6 +44,9 @@ gem "recaptcha", require: "recaptcha/rails"
 # Mailgun
 gem 'mailgun-ruby', '~>1.1.6'
 
+# Omniauth
+gem 'omniauth-google-oauth2'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "documents#index"
   resources :documents
-  devise_for :users, controllers: { registrations: "registrations", passwords: "passwords" }
+  devise_for :users, controllers: { registrations: "registrations", passwords: "passwords", omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
